36ad5d849de141c767a010aac2ad0995c4940cc5
Raphael Borba <raphaelbsx@gmail.com>
2025-09-26 11:58:10 -0300

    feat(redis): save categoies is cache with redis

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,12 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CategoriesRepository } from './categories.repository';
+import { REDIS_CLIENT } from 'src/redis/redis.module';
+import type Redis from 'ioredis';
+
+const CATEGORIES_CACHE_TTL_SECONDS = 300;
 
 @Injectable()
 export class CategoriesService {
-  constructor(private readonly repo: CategoriesRepository) {}
+  constructor(private readonly repo: CategoriesRepository,
+    @Inject(REDIS_CLIENT) private readonly redis: Redis,
+
+  ) {}
 
   async getAll() {
+    const cacheKey = 'categories';
+    const cached = await this.redis.get(cacheKey);
+    if (cached) return JSON.parse(cached);
+
     const docs = await this.repo.findAllLean();
+    await this.redis.set(cacheKey, JSON.stringify(docs), 'EX', CATEGORIES_CACHE_TTL_SECONDS);
     return docs.map((c) => ({ id: c._id, name: c.name }));
   }
 }
